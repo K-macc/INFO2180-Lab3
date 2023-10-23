58167d9a90be6625f878110cea4076835ce8eb2d
K-macc <mkeshawn107@gmail.com>
2023-10-22 21:03:19 -0500

    Added clicking functionality

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,16 +1,49 @@
-document.addEventListener('DOMContentLoaded',function(create){
+document.addEventListener('DOMContentLoaded', function (create) {
     const boxes = document.querySelectorAll("#board div");
-    boxes.forEach((square)=>{
-        square.classList.add('square');
-    })
+    const status = document.querySelectorAll("#status div");
+    //console.log(status);
+    var check = [];
+    var n = 0;
+    let choice = function () {
+        if (n % 2 == 0) {
+            n += 1;
+            return "X";
+        } else {
+            n += 1;
+            return "O";
+        }
+    }
 
-    boxes.forEach(function(el,ind){
-        el.addEventListener('mouseover', function(e) {
+    let colour = function(c){
+        if(c == "X"){
+
+        }
+    }
+    boxes.forEach((square, index) => {
+        //var n = 0;
+        square.classList.add('square');
+
+        square.addEventListener('mouseover', function (e) {
             e.target.classList.add('hover');
         })
 
-        el.addEventListener('mouseout', function(e) {
+        square.addEventListener('mouseout', function (e) {
             e.target.classList.remove('hover');
+        })
+
+        square.addEventListener('click', function (e) {
+           if(choice() == "X"){
+                e.target.textContent = "X";
+                e.target.classList.add('X');
+                check.push("X");
+                console.log(check);
+           }else{
+                e.target.textContent = "O";
+                e.target.classList.add('O');
+                check.push("O");
+                console.log(check);
+           }
+
         })
     })
 })
